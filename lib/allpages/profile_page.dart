7b22de873387315aaebe864c9cc7a1e8bcb2fa87
sumanhope <sumansthahope@gmail.com>
2023-01-14 23:58:54 +0545

    Profile page UI

--- lib/allpages/profile_page.dart
+++ lib/allpages/profile_page.dart
@@ -1,4 +1,9 @@
+// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
+
+import 'package:firebase_auth/firebase_auth.dart';
 import "package:flutter/material.dart";
+
+import '../Welcome/welcome_page.dart';
 
 class ProfileScreen extends StatefulWidget {
   const ProfileScreen({super.key});
@@ -14,21 +19,194 @@ class _ProfileScreenState extends State<ProfileScreen> {
       appBar: AppBar(
         title: const Text("Profile"),
       ),
-      body: Column(
-        children: [
-          const SizedBox(
-            height: 115,
-            width: 115,
-            child: CircleAvatar(
-              backgroundImage: AssetImage("images/kayo.jpg"),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            SizedBox(
+              height: 20,
             ),
+            Row(
+              children: [
+                SizedBox(
+                  height: 115,
+                  width: 150,
+                  child: CircleAvatar(
+                    backgroundImage: AssetImage("images/kayo.jpg"),
+                  ),
+                ),
+                Stack(
+                  children: [
+                    Text(
+                      "Name",
+                      style: TextStyle(
+                        fontSize: 15,
+                        color: Colors.teal,
+                        fontWeight: FontWeight.bold,
+                        fontFamily: 'Poppins',
+                      ),
+                    ),
+                    Container(
+                      margin: EdgeInsets.only(top: 30.0, right: 10.0),
+                      child: Text(
+                        "Age",
+                        style: TextStyle(
+                          fontSize: 15,
+                          color: Colors.teal,
+                          fontWeight: FontWeight.bold,
+                          fontFamily: 'Poppins',
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+                Spacer(),
+                Stack(
+                  children: [
+                    Container(
+                      margin: EdgeInsets.only(bottom: 60.0, right: 12.0),
+                      child: ElevatedButton(
+                        onPressed: () {
+                          FirebaseAuth.instance.signOut().then((value) {
+                            Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                builder: ((context) => const WelcomeScreen()),
+                              ),
+                            );
+                          });
+                        },
+                        child: Text(
+                          "Logout",
+                          style: TextStyle(
+                            fontSize: 15,
+                            color: Colors.white,
+                            fontWeight: FontWeight.bold,
+                            fontFamily: 'Poppins',
+                          ),
+                        ),
+                      ),
+                    ),
+                    Container(
+                        margin: EdgeInsets.only(top: 60.0, left: 20.0),
+                        child: TextButton(
+                          child: Icon(
+                            Icons.edit_note,
+                            size: 40,
+                            color: Colors.teal,
+                          ),
+                          onPressed: () {},
+                        )),
+                  ],
+                ),
+              ],
+            ),
+            SizedBox(height: 20),
+            Divider(
+              height: 5,
+              thickness: 2,
+              color: Colors.black,
+            ),
+            ProfileMenu(
+              firsticon: Icons.person,
+              text: "Username",
+              secondicon: Icons.chevron_right_sharp,
+              press: () {},
+            ),
+            ProfileMenu(
+              firsticon: Icons.favorite,
+              text: "Favorites",
+              secondicon: Icons.chevron_right_sharp,
+              press: () {},
+            ),
+            ProfileMenu(
+              firsticon: Icons.camera_alt,
+              text: "Before and After",
+              secondicon: Icons.chevron_right_sharp,
+              press: () {},
+            ),
+            ProfileMenu(
+              firsticon: Icons.settings,
+              text: "App settings",
+              secondicon: Icons.chevron_right_sharp,
+              press: () {},
+            ),
+            ProfileMenu(
+              firsticon: Icons.security,
+              text: "Password and Security",
+              secondicon: Icons.chevron_right_sharp,
+              press: () {},
+            ),
+            ProfileMenu(
+              firsticon: Icons.notifications,
+              text: "Notification",
+              secondicon: Icons.chevron_right_sharp,
+              press: () {},
+            ),
+            ProfileMenu(
+              firsticon: Icons.share,
+              text: "Share with your friends",
+              secondicon: Icons.chevron_right_sharp,
+              press: () {},
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class ProfileMenu extends StatelessWidget {
+  final String text;
+  final IconData firsticon, secondicon;
+  final VoidCallback press;
+  const ProfileMenu({
+    Key? key,
+    required this.text,
+    required this.firsticon,
+    required this.secondicon,
+    required this.press,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 10),
+      child: TextButton(
+        style: TextButton.styleFrom(
+          padding: EdgeInsets.all(15),
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(8),
           ),
-          const SizedBox(height: 20),
-          Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
-            child: TextButton(onPressed: () {}, child: const Text("Username")),
-          ),
-        ],
+          backgroundColor: Color.fromARGB(255, 219, 223, 222),
+        ),
+        onPressed: press,
+        child: Row(
+          // crossAxisAlignment: CrossAxisAlignment.start,
+          // mainAxisAlignment: MainAxisAlignment.start,
+          children: [
+            Icon(
+              firsticon,
+              size: 25,
+            ),
+            SizedBox(
+              width: 10,
+            ),
+            Text(
+              text,
+              style: TextStyle(
+                fontSize: 18,
+                color: Colors.teal,
+                fontWeight: FontWeight.bold,
+                fontFamily: 'Poppins',
+              ),
+            ),
+            Spacer(),
+            Icon(
+              secondicon,
+              size: 30,
+            ),
+          ],
+        ),
       ),
     );
   }
