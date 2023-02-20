329899e1c6ad0450c5a0e0d23aa73108f1e3eacc
sumanhope <sumansthahope@gmail.com>
2023-02-20 20:12:32 +0545

    Search page and nutritional page UI

--- lib/allpages/nutritional.dart
+++ lib/allpages/nutritional.dart
@@ -20,6 +20,7 @@ class _ViewDataState extends State<ViewData> {
   String foodDesc = "";
   String servSize = "0";
   String imageurl = "";
+  bool isfav = false;
 
   @override
   void initState() {
@@ -38,7 +39,26 @@ class _ViewDataState extends State<ViewData> {
       foodDesc = foodDoc.get('foodDescription');
       servSize = foodDoc.get('servingSize');
       calories = foodDoc.get('calories');
+      isfav = foodDoc.get('favorite');
     });
+  }
+
+  Future<void> favtoggle() async {
+    setState(() {
+      if (isfav) {
+        isfav = false;
+      } else {
+        isfav = true;
+      }
+    });
+    final usercollection = FirebaseFirestore.instance.collection('food');
+    final docRef = usercollection.doc(widget.foodId);
+
+    try {
+      await docRef.update({"favorite": isfav});
+    } catch (e) {
+      debugPrint("some error occured $e");
+    }
   }
 
   @override
@@ -67,161 +87,204 @@ class _ViewDataState extends State<ViewData> {
         ),
       ),
       body: SafeArea(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Padding(
-              padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 20),
-              child: Container(
-                  //alignment: Alignment.center,
-                  width: 390,
-                  height: 250,
-                  decoration: BoxDecoration(
-                    image: imageurl != ""
-                        ? DecorationImage(
-                            image: NetworkImage(imageurl),
-                            fit: BoxFit.fill,
-                          )
-                        : const DecorationImage(
-                            image: AssetImage("images/box.png"),
-                            fit: BoxFit.cover,
-                          ),
-                    //color: const Color.fromARGB(94, 68, 137, 255),
-                  )),
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 20),
-                  child: Text(
-                    widget.foodname,
-                    style: const TextStyle(
-                      letterSpacing: 1.5,
-                      fontSize: 18,
-                      color: Colors.teal,
-                      fontWeight: FontWeight.bold,
-                      fontFamily: 'Poppins',
-                    ),
-                  ),
-                ),
-                Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 20),
-                  child: IconButton(
-                    onPressed: () {},
-                    icon: const Icon(Icons.favorite),
-                    iconSize: 30,
-                    color: Colors.teal,
-                  ),
-                ),
-              ],
-            ),
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 20),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
+        child: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Padding(
+                padding:
+                    const EdgeInsets.symmetric(vertical: 10, horizontal: 20),
+                child: Container(
+                    //alignment: Alignment.center,
+                    width: 390,
+                    height: 250,
+                    decoration: BoxDecoration(
+                      image: imageurl != ""
+                          ? DecorationImage(
+                              image: NetworkImage(imageurl),
+                              fit: BoxFit.fill,
+                            )
+                          : const DecorationImage(
+                              image: AssetImage("images/box.png"),
+                              fit: BoxFit.cover,
+                            ),
+                      //color: const Color.fromARGB(94, 68, 137, 255),
+                    )),
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 children: [
-                  Text(
-                    "Food Category: $foodCat",
-                    style: const TextStyle(
-                      letterSpacing: 1.5,
-                      fontSize: 14,
-                      color: Colors.teal,
-                      fontWeight: FontWeight.bold,
-                      fontFamily: 'Poppins',
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 20),
+                    child: Text(
+                      widget.foodname,
+                      style: const TextStyle(
+                        letterSpacing: 1.7,
+                        fontSize: 20,
+                        color: Colors.teal,
+                        fontWeight: FontWeight.bold,
+                        fontFamily: 'Poppins',
+                      ),
                     ),
                   ),
-                  box,
-                  Text(
-                    "Serving Size: $servSize",
-                    style: const TextStyle(
-                      letterSpacing: 1.5,
-                      fontSize: 14,
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 20),
+                    child: IconButton(
+                      onPressed: favtoggle,
+                      icon: (isfav
+                          ? const Icon(Icons.favorite)
+                          : const Icon(Icons.favorite_border)),
+                      iconSize: 30,
                       color: Colors.teal,
-                      fontWeight: FontWeight.bold,
-                      fontFamily: 'Poppins',
-                    ),
-                  ),
-                  box,
-                  Text(
-                    "Food Description: $foodDesc",
-                    style: const TextStyle(
-                      letterSpacing: 1.5,
-                      fontSize: 14,
-                      color: Colors.teal,
-                      fontWeight: FontWeight.bold,
-                      fontFamily: 'Poppins',
-                    ),
-                  ),
-                  box,
-                  Text(
-                    "Calories: $calories",
-                    style: const TextStyle(
-                      letterSpacing: 1.5,
-                      fontSize: 14,
-                      color: Colors.teal,
-                      fontWeight: FontWeight.bold,
-                      fontFamily: 'Poppins',
                     ),
                   ),
                 ],
               ),
-            ),
-            const SizedBox(
-              height: 10,
-            ),
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 20),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  const Text(
-                    "Nutritional Facts",
-                    style: TextStyle(
-                      letterSpacing: 1.5,
-                      fontSize: 18,
-                      color: Colors.teal,
-                      fontWeight: FontWeight.bold,
-                      fontFamily: 'Poppins',
-                    ),
-                  ),
-                  box,
-                  const Text(
-                    "Vitam A: ",
-                    style: TextStyle(
-                      letterSpacing: 1.5,
-                      fontSize: 14,
-                      color: Colors.teal,
-                      fontWeight: FontWeight.bold,
-                      fontFamily: 'Poppins',
-                    ),
-                  ),
-                  box,
-                  const Text(
-                    "Total Crab:",
-                    style: TextStyle(
-                      letterSpacing: 1.5,
-                      fontSize: 14,
-                      color: Colors.teal,
-                      fontWeight: FontWeight.bold,
-                      fontFamily: 'Poppins',
-                    ),
-                  ),
-                  box,
-                  const Text(
-                    "Iron: ",
-                    style: TextStyle(
-                      letterSpacing: 1.5,
-                      fontSize: 14,
-                      color: Colors.teal,
-                      fontWeight: FontWeight.bold,
-                      fontFamily: 'Poppins',
-                    ),
-                  ),
-                ],
+              const Divider(
+                thickness: 2,
+                indent: 14,
+                endIndent: 14,
+                color: Colors.teal,
+                height: 5,
               ),
-            ),
-          ],
+              const SizedBox(
+                height: 5,
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 20),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      "Food Category: $foodCat",
+                      style: const TextStyle(
+                        letterSpacing: 1.5,
+                        fontSize: 14,
+                        color: Colors.teal,
+                        fontWeight: FontWeight.bold,
+                        fontFamily: 'Poppins',
+                      ),
+                    ),
+                    box,
+                    Text(
+                      "Serving Size: $servSize",
+                      style: const TextStyle(
+                        letterSpacing: 1.5,
+                        fontSize: 14,
+                        color: Colors.teal,
+                        fontWeight: FontWeight.bold,
+                        fontFamily: 'Poppins',
+                      ),
+                    ),
+                    box,
+                    Text(
+                      "Food Description: $foodDesc",
+                      style: const TextStyle(
+                        letterSpacing: 1.5,
+                        fontSize: 14,
+                        color: Colors.teal,
+                        fontWeight: FontWeight.bold,
+                        fontFamily: 'Poppins',
+                      ),
+                    ),
+                    box,
+                    Text(
+                      "Calories: $calories",
+                      style: const TextStyle(
+                        letterSpacing: 1.5,
+                        fontSize: 14,
+                        color: Colors.teal,
+                        fontWeight: FontWeight.bold,
+                        fontFamily: 'Poppins',
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              const Divider(
+                thickness: 2,
+                indent: 14,
+                endIndent: 14,
+                color: Colors.teal,
+              ),
+              // const SizedBox(
+              //   height: 10,
+              // ),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 20),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    const Text(
+                      "Nutritional Facts",
+                      style: TextStyle(
+                        letterSpacing: 1.5,
+                        fontSize: 18,
+                        color: Colors.teal,
+                        fontWeight: FontWeight.bold,
+                        fontFamily: 'Poppins',
+                      ),
+                    ),
+                    box,
+                    const Text(
+                      "Vitam A: ",
+                      style: TextStyle(
+                        letterSpacing: 1.5,
+                        fontSize: 14,
+                        color: Colors.teal,
+                        fontWeight: FontWeight.bold,
+                        fontFamily: 'Poppins',
+                      ),
+                    ),
+                    box,
+                    const Text(
+                      "Total Crab:",
+                      style: TextStyle(
+                        letterSpacing: 1.5,
+                        fontSize: 14,
+                        color: Colors.teal,
+                        fontWeight: FontWeight.bold,
+                        fontFamily: 'Poppins',
+                      ),
+                    ),
+                    box,
+                    const Text(
+                      "Iron: ",
+                      style: TextStyle(
+                        letterSpacing: 1.5,
+                        fontSize: 14,
+                        color: Colors.teal,
+                        fontWeight: FontWeight.bold,
+                        fontFamily: 'Poppins',
+                      ),
+                    ),
+                    box,
+                    const Text(
+                      "Sugar: ",
+                      style: TextStyle(
+                        letterSpacing: 1.5,
+                        fontSize: 14,
+                        color: Colors.teal,
+                        fontWeight: FontWeight.bold,
+                        fontFamily: 'Poppins',
+                      ),
+                    ),
+                    box,
+                    const Text(
+                      "Vitamin D: ",
+                      style: TextStyle(
+                        letterSpacing: 1.5,
+                        fontSize: 14,
+                        color: Colors.teal,
+                        fontWeight: FontWeight.bold,
+                        fontFamily: 'Poppins',
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
--- lib/allpages/search_page.dart
+++ lib/allpages/search_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:keyboard_dismisser/keyboard_dismisser.dart';
 
 class SearchPage extends StatefulWidget {
   const SearchPage({super.key});
@@ -8,20 +9,153 @@ class SearchPage extends StatefulWidget {
 }
 
 class _SearchPageState extends State<SearchPage> {
-  @override
-  Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text(
-          "Search page",
-          style: TextStyle(
-            fontSize: 15,
-            color: Colors.teal,
-            fontWeight: FontWeight.bold,
-            fontFamily: 'Poppins',
-          ),
+  final searchcontroller = TextEditingController();
+  Widget buildUser() {
+    return TextField(
+      controller: searchcontroller,
+      decoration: InputDecoration(
+        filled: true,
+        fillColor: const Color.fromARGB(117, 100, 255, 219),
+        enabledBorder: const OutlineInputBorder(
+          borderRadius: BorderRadius.all(Radius.circular(8)),
+          borderSide:
+              BorderSide(color: Color.fromRGBO(77, 182, 172, 1), width: 3),
+        ),
+        labelText: 'Search',
+        labelStyle: const TextStyle(
+          color: Colors.teal,
+          fontSize: 15,
+          fontWeight: FontWeight.bold,
+          fontFamily: 'Poppins',
+        ),
+        prefixIcon: const Icon(
+          Icons.search,
+          color: Colors.teal,
+          size: 30,
+        ),
+        suffixIcon: searchcontroller.text.isEmpty
+            ? Container(width: 0)
+            : IconButton(
+                icon: const Icon(Icons.close),
+                onPressed: () => searchcontroller.clear(),
+              ),
+        focusedBorder: const OutlineInputBorder(
+          borderRadius: BorderRadius.all(Radius.circular(8)),
+          borderSide: BorderSide(color: Colors.teal, width: 3),
         ),
       ),
+      onChanged: (value) => _runFilter(value),
+      style: const TextStyle(
+        color: Colors.black,
+        fontSize: 15,
+        fontFamily: 'Poppins',
+      ),
+      keyboardType: TextInputType.text,
+      textInputAction: TextInputAction.done,
     );
   }
+
+  final List<Map<String, dynamic>> _allUsers = [
+    {"id": 1, "foodname": "Rice", "cal": 223},
+    {"id": 2, "foodname": "Noodles", "cal": 159},
+    // {"id": 3, "foodname": "Potato", "cal": 220},
+    {"id": 4, "foodname": "Banana", "cal": 105},
+    {"id": 5, "foodname": "Orange", "cal": 65},
+    {"id": 6, "foodname": "Watermelon", "cal": 50},
+    {"id": 7, "foodname": "Peas,green", "cal": 63},
+    {"id": 8, "foodname": "Bread", "cal": 61},
+    {"id": 9, "foodname": "Pizza", "cal": 290},
+    {"id": 10, "foodname": "Sweet Potato", "cal": 118},
+  ];
+
+  // This list holds the data for the list view
+  List<Map<String, dynamic>> _foundUsers = [];
+  @override
+  initState() {
+    _foundUsers = _allUsers;
+    super.initState();
+  }
+
+  // This function is called whenever the text field changes
+  void _runFilter(String enteredKeyword) {
+    List<Map<String, dynamic>> results = [];
+    if (enteredKeyword.isEmpty) {
+      // if the search field is empty or only contains white-space, we'll display all users
+      results = _allUsers;
+    } else {
+      results = _allUsers
+          .where((user) => user["foodname"]
+              .toLowerCase()
+              .contains(enteredKeyword.toLowerCase()))
+          .toList();
+      // we use the toLowerCase() method to make it case-insensitive
+    }
+    setState(() {
+      _foundUsers = results;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) => KeyboardDismisser(
+        gestures: const [
+          GestureType.onTap,
+          GestureType.onPanUpdateDownDirection,
+        ],
+        child: Scaffold(
+          appBar: AppBar(
+            automaticallyImplyLeading: false,
+            title: const Text(
+              "Search",
+              style: TextStyle(
+                fontSize: 18,
+                color: Colors.white,
+                fontWeight: FontWeight.bold,
+                fontFamily: 'Poppins',
+              ),
+            ),
+            centerTitle: true,
+          ),
+          body: SafeArea(
+            child: Column(
+              children: [
+                const SizedBox(
+                  height: 10,
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: buildUser(),
+                ),
+                Expanded(
+                  child: _foundUsers.isNotEmpty
+                      ? ListView.builder(
+                          itemCount: _foundUsers.length,
+                          itemBuilder: (context, index) => Card(
+                            key: ValueKey(_foundUsers[index]["id"]),
+                            color: const Color.fromARGB(153, 0, 150, 135),
+                            elevation: 9,
+                            shadowColor: Colors.teal[800],
+                            child: ListTile(
+                              leading: Text(
+                                _foundUsers[index]["id"].toString(),
+                                style: const TextStyle(
+                                    fontSize: 24, color: Colors.white),
+                              ),
+                              title: Text(_foundUsers[index]['foodname'],
+                                  style: const TextStyle(color: Colors.white)),
+                              subtitle: Text(
+                                  '${_foundUsers[index]["cal"].toString()} cal',
+                                  style: const TextStyle(color: Colors.white)),
+                            ),
+                          ),
+                        )
+                      : const Text(
+                          'No results found',
+                          style: TextStyle(fontSize: 24),
+                        ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      );
 }
